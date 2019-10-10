130edab1b83adb501b319d722ccc3e4d7ce0bb2e
Antoine Williams <54339074+antoineel9@users.noreply.github.com>
2019-10-09 21:08:37 -0500

    Add files via upload

---
+++ Dinosaurus.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JurassicWorld.Data
+{
+    public class Dinosaurus
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int DID { get; set; }
+        public string DName { get; set; }
+            [ForeignKey("EID")]
+        public Exhibits ExhibitsId{ get; set; }
+
+
+    }
+}
---
+++ Exhibits.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JurassicWorld.Data
+{
+    public class Exhibits
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int EID { get; set; }
+        [Display(Name= "Exhibits Name")]
+        public string  EName { get; set; }
+        [Display(Name = "Exhibits Head")]
+        public string EHead{ get; set; }
+    }
+}
